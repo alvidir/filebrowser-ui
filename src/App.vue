a8578cd39f0cda86a083440fb4458cd6a95e5244
HectorMRC <thehector1593@gmail.com>
2022-05-08 03:20:07 +0200

    feat: added theme switch

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,68 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+const THEME_LIGHT = "theme-light";
+const THEME_DARK = "theme-dark";
+
 export default defineComponent({
   name: "App",
   components: {},
+
+  data() {
+    return {
+      theme: THEME_LIGHT,
+    }
+  },
+
+  mounted() {
+    const setLightTheme = () => {
+      document.getElementsByTagName('body')[0].classList.add('theme-light')
+      localStorage.setItem(process.env.VUE_APP_THEME_STORAGE_KEY, THEME_LIGHT)
+      this.theme = THEME_LIGHT
+    }
+
+    const setDarkTheme = () => {
+      document.getElementsByTagName('body')[0].classList.add('theme-dark')
+      localStorage.setItem(process.env.VUE_APP_THEME_STORAGE_KEY, THEME_DARK)
+      this.theme = THEME_DARK
+    }
+
+    var localTheme = localStorage.getItem(process.env.VUE_APP_THEME_STORAGE_KEY)
+    if (localTheme === THEME_LIGHT) {
+      setLightTheme()
+    } else if (localTheme === THEME_DARK) {
+      setDarkTheme()
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setDarkTheme()
+    } else {
+      setLightTheme()
+    }
+  }
 });
 </script>
 
 <style lang="scss">
+@import "fibonacci-styles";
+
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Raleway", Helvetica, Arial, sans-serif;
+}
+
+body {
+  min-height: 100vh;
+  width: 100%;
+  background: var(--color-background-secondary);
+}
+
+.centered-column {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
