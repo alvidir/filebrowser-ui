18dc606b3e45c097d7b5108a1629619bdf0263c8
Hector <thehector1593@gmail.com>
2022-12-31 15:20:32 +0100

    chore: move directive to external repo

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { include as includeFields } from "vue-fields/src/main";
 import { include as includeDock } from "vue-dock/src/main";
 import { include as includeButtons } from "vue-buttons/src/main";
 import { include as includeCardsComponents } from "vue-cards/src/main";
-import clickOutside from "./directives/ClickOutside";
+import { include as includeDirectives } from "vue-directives/src/main";
 
 const app = createApp(App);
 includeCardsComponents(app);
 includeButtons(app);
 includeFields(app);
 includeDock(app);
+includeDirectives(app);
 
-app.directive("click-outside", clickOutside);
 app.mount("#app");
